5b0d9c4f9c4f5440ee2d5bd04194483e8f7cfc5f
LopezFarm <60264690+Yafet4275@users.noreply.github.com>
2023-04-20 16:56:12 -0500

    Adding another way
    I added different code to do the same thing on app.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
+// const itemInput = document.getElementById('itemInput');
+// const addButton = document.getElementById('addButton');
+// const itemList = document.getElementById('itemList');
+
+// let items = [];
+
+// addButton.addEventListener('click', function() {
+// if (itemInput.value !== '') {
+//     items.push(itemInput.value);
+//     itemInput.value = '';
+//     renderList();
+// }
+// });
+
+// function renderList() {
+// itemList.innerHTML = '';
+// for (let i = 0; i < items.length; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = items[i];
+//     itemList.appendChild(li);
+// }
+// }
+
+
 const itemInput = document.getElementById('itemInput');
 const addButton = document.getElementById('addButton');
 const itemList = document.getElementById('itemList');
 
-let items = [];
-
 addButton.addEventListener('click', function() {
-if (itemInput.value !== '') {
-    items.push(itemInput.value);
-    itemInput.value = '';
-    renderList();
-}
-});
-
-function renderList() {
-itemList.innerHTML = '';
-for (let i = 0; i < items.length; i++) {
+  const item = itemInput.value;
+  if (item !== '') {
     const li = document.createElement('li');
-    li.textContent = items[i];
+    li.textContent = item;
     itemList.appendChild(li);
-}
-}
+    itemInput.value = '';
+  }
+});
